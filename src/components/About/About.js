27d2d3db06d8dbf1ba4793465ed151baef9dd694
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-14 10:21:54 +0100

    Update: update content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              Know Who <strong className="purple">I am</strong>
             </h1>
             <Aboutcard />
           </Col>
@@ -35,7 +35,6 @@ function About() {
         </h1>
 
         <Techstack />
-
       </Container>
     </Container>
   );
