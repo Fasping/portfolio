fb3f5d1ca503f48f7612b8d35347e87e96ab51c4
Fernando Cases <nandocasesgarcia@gmail.com>
2023-04-24 14:13:13 +0200

    Feat: update projects

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiCss3,
-  DiSass
-} from "react-icons/di";
-
+import { DiJsBadge, DiReact, DiCss3, DiSass, DiGit } from "react-icons/di";
+import { SiTypescript, SiJest } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <DiJsBadge />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTypescript />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiSass />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJest />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
       </Col>
     </Row>
   );
